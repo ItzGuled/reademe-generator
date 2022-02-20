f673d1bbcedbab0b95d8198d731fe093ef5d4382
itzguled <itzguled55337@gmail.com>
2022-02-19 20:53:58 -0600

    gen

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,8 +13,17 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ==========================================
-  
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Installation](#installation)
 `;
 }
 
